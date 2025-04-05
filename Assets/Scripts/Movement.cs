0a08545877495576fe531d7aeb61fd9c18cd2570
TheBigLin <Hayden22@live.ca>
2025-04-05 13:50:03 -0400

    Added the basics

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        transform.position += direction * 8.0f * Time.deltaTime;
+    }
+}
